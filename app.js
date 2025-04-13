abfbceb4ef95a4677582c5c9e26653ac49921464
Mitiku tadesse <149384297+Mitiku14@users.noreply.github.com>
2025-04-13 19:35:51 +0300

    Added views folder and updated routes

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
 
 const connectDB = require('./src/config/db');
 require('./src/config/passport');
 
+// Import Routes
 const authRoute = require('./src/routes/authRoutes');
 const postRoute = require('./src/routes/postRoutes');
 const profileRoute = require('./src/routes/profileRoutes');
 const googleAuthRoute = require('./src/routes/googleAuthRoutes');
 
 const app = express();
-
-
 const PORT = process.env.PORT || 5000;
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+// Middleware
 app.use(express.json());
 app.use(
   session({
@@ -23,21 +29,28 @@ app.use(
     saveUninitialized: false,
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Connect to Database
 connectDB();
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.use('/api/auth', authRoute);
 app.use('/api/post', postRoute);
 app.use('/api/profile', profileRoute);
-app.use('/auth/', googleAuthRoute);
+app.use('/auth', googleAuthRoute);
 
-// Catch-all route for requests that don't match any defined routes
+// 404 Handler (Catch-all)
 app.use((req, res) => {
   res.status(404).json({ msg: 'Route not found' });
 });
 
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
